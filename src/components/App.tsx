99fab27ff5ddd5862b03aba952222a6f54ee0225
Zacharias Knudsen <zachasme@gmail.com>
2019-08-19 15:25:43 +0200

    Prettier

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,23 +1,8 @@
 import React from "react";
 
-import { i18n } from "@lingui/core";
-import { I18nProvider } from "@lingui/react";
-
 import CheatSheet from "./CheatSheet";
 import "./App.css";
 
-async function loadCatalog(locale: string) {
-  const catalog = await import(`@lingui/loader!../locales/${locale}.po`);
-  i18n.load(locale, catalog);
-}
-
-i18n.on("activate", loadCatalog);
-i18n.activate("en");
-
 export default () => {
-  return (
-    <I18nProvider i18n={i18n}>
-      <CheatSheet />
-    </I18nProvider>
-  );
+  return <CheatSheet />;
 };
